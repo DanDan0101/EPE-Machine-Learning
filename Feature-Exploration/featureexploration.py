85cfe1c68b001149cd21574ed02e804a00eeeb4b
Daniel Sun <danielcsun@hotmail.com>
2020-07-22 16:43:29 -0700

    Code reorganization + Task 2
    Created Jet class and associated methods, cleaned up code for Task 1, added code framework for Task 2

--- Feature-Exploration/featureexploration.py
+++ Feature-Exploration/featureexploration.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from matplotlib import pyplot as plt
 import h5py
 
 # Read data file
@@ -8,21 +9,46 @@ data_path = os.path.join(dirname, 'processed-pythia82-lhc13-all-pt1-50k-r1_h022_
 f = h5py.File(data_path, 'r')
 dset = f['t_allpar_new']
 
-def countjets():
-    g = dset["j_g"].sum()
-    q = dset["j_q"].sum()
-    w = dset["j_w"].sum()
-    z = dset["j_z"].sum()
-    t = dset["j_t"].sum()
-    undef = dset["j_undef"].sum()
-    
-    # Final results
-    print("Total: " + str(g + q + w + z + t + undef))
-    print("q: " + str(q))
-    print("g: " + str(g))
-    print("w: " + str(w))
-    print("z: " + str(z))
-    print("t: " + str(t))
-    print("undef: " + str(undef))
+class Jet:
+    def __init__(self, name, abbr, color):
+        self.name = name
+        self.abbr = abbr
+        self.color = color
+    def count(self):
+        """Returns a count of the number of instance of a specific jet type."""
+        return dset[self.abbr].sum()
+    def get_property(self, property):
+        """Returns a 1-D numpy array of all properties of a specific jet type."""
+        return dset[property][dset[self.abbr] != 0]
 
-# countjets()
+q = Jet("quark", "j_q", "tab:blue")
+g = Jet("gluon", "j_g", "tab:orange")
+w = Jet("W", "j_w", "tab:green")
+z = Jet("Z", "j_z", "tab:red")
+t = Jet("top", "j_t", "tab:purple")
+undef = Jet("undefined", "j_undef", "k")
+
+jets = np.array([q, g, w, z, t, undef])
+total = len(dset)
+
+def countjets():
+    print("Total: " + str(total))
+    for jet in jets:
+        print(jet.name + ": " + str(jet.count()))
+
+def makeplot(property):
+    plt.xlabel(property)
+    plt.ylabel("Prob. Density (a.u.)")
+    for jet in jets:
+        properties = jet.get_property(property)
+        plt.hist(properties, bins = 100, range = (dset[property].min(), dset[property].max()), weights = np.full_like(properties, 1 / total), histtype = "step", color = jet.color, label = jet.name)
+    plt.legend()
+    plt.show()
+
+jet_kinematics = ["j_mass_mmdt", "j_pt", "j_eta"]
+jet_substructures = ["j_zlogz", "j_multiplicity"]
+ecf_c1 = ["j_c1_b0_mmdt", "j_c1_b1_mmdt", "j_c1_b2_mmdt"]
+ecf_c2 = ["j_c2_b1_mmdt", "j_c2_b2_mmdt"]
+ecf_d2 = ["j_d2_b1_mmdt", "j_d2_b2_mmdt", "j_d2_a1_b1_mmdt", "j_d2_a1_b2_mmdt"]
+ecf_m2 = ["j_m2_b1_mmdt", "j_m2_b2_mmdt"]
+ecf_n2 = ["j_n2_b1_mmdt", "j_n2_b2_mmdt"]
